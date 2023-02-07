427226bdddfc2c617fefca6abcd7fb94fc1d43fa
Pravith <pravith10@gmail.com>
2023-02-07 22:03:26 +0530

    Features: 1. Added my detauls in data.js 2. Added resume

--- src/data.js
+++ src/data.js
@@ -1,14 +1,16 @@
 // Skills icons - https://react-icons.github.io/react-icons/
 import {
-  FaHtml5,
-  FaCss3Alt,
-  FaBootstrap,
   FaReact,
   FaGitAlt,
-  FaGithubSquare,
   FaLink,
+  FaPython,
+  FaAws,
+  FaDocker,
+  FaAngular,
+  FaRust,
+  FaNodeJs,
 } from "react-icons/fa";
-import { SiJavascript, SiStyledcomponents, SiRedux } from "react-icons/si";
+import { SiTypescript } from "react-icons/si";
 
 // Projects Images (add your images to the images directory and import below)
 import Logo from "./images/logo.svg";
@@ -24,7 +26,7 @@ export { HeroDark as Dark };
  **************************************************************
   Add your GitHub username (string - "YourUsername") below.
 */
-export const githubUsername = "Your GitHub username here";
+export const githubUsername = "PravithBA";
 
 // Blog link icon (imported above)
 export const Blog = <FaLink />;
@@ -34,7 +36,7 @@ export const Blog = <FaLink />;
   Add a second paragraph for the about me section.
 */
 export const moreInfo =
-  "I enjoy learning about technology and helping others use it to improve their lives and be more productive. I built this site with React, Bootstrap, Redux, and the GitHub REST API.";
+  "I am a driven programmer with a love for problem-solving and taking on challenges. Skilled in multiple programming languages, I am dedicated to delivering high-quality, innovative solutions. I stay current with industry advancements and continuously improve my skills. In my free time, I enjoy experimenting with new tools. I am passionate about technology and its potential to make a positive impact. I thrive in a fast-paced environment and am always eager for new projects.";
 
 /* Skills
  ************************************************************** 
@@ -42,67 +44,63 @@ export const moreInfo =
 */
 export const skillData = [
   {
-    id: 1,
-    skill: <FaHtml5 className="display-4" />,
-    name: "HTML5",
-  },
-  {
-    id: 2,
-    skill: <FaCss3Alt className="display-4" />,
-    name: "CSS3",
-  },
-  {
-    id: 3,
-    skill: <SiJavascript className="display-4" />,
-    name: "JavaScript",
-  },
-  {
-    id: 4,
-    skill: <FaBootstrap className="display-4" />,
-    name: "BootStrap",
+    id: 13,
+    skill: <FaAngular className="display-5" />,
+    name: "Angular",
   },
   {
     id: 5,
-    skill: <FaReact className="display-4" />,
+    skill: <FaReact className="display-5" />,
     name: "React",
   },
   {
-    id: 6,
-    skill: <SiStyledcomponents className="display-4" />,
-    name: "Styled Components",
+    id: 7,
+    skill: <SiTypescript className="display-5" />,
+    name: "Typescript",
   },
   {
-    id: 7,
-    skill: <SiRedux className="display-4" />,
-    name: "Redux",
+    id: 12,
+    skill: <FaNodeJs className="display-5" />,
+    name: "NodeJs",
+  },
+  {
+    id: 13,
+    skill: <FaRust className="display-5" />,
+    name: "Rust",
+  },
+  {
+    id: 10,
+    skill: <FaPython className="display-5" />,
+    name: "Python",
   },
   {
     id: 8,
-    skill: <FaGitAlt className="display-4" />,
+    skill: <FaGitAlt className="display-5" />,
     name: "Git",
   },
   {
-    id: 9,
-    skill: <FaGithubSquare className="display-4" />,
-    name: "GitHub",
+    id: 11,
+    skill: <FaAws className="display-5" />,
+    name: "AWS",
+  },
+  {
+    id: 12,
+    skill: <FaDocker className="display-5" />,
+    name: "Docker",
   },
 ];
 
 // Resume link (string - "https://YourResumeUrl") - I am using CloudFront to share my resume (https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/Introduction.html)
-export const resume = null;
+export const resume = "https://raw.githubusercontent.com/PravithBA/personal-website/main/public/MyResume.pdf";
 
 /* Projects
  ************************************************************** 
   List the repo names (string - "your-repo-name") you want to include (they will be sorted alphabetically). If empty, only the first 3 will be included.
 */
-export const filteredProjects = ["example-1", "example-2", "example-3"];
+export const filteredProjects = ["beetee-css", "DraggeDro", "Youtube_Shorts_Remover_Browser_Extension"];
 
 // Replace the defualt GitHub image for matching repos below (images imported above - lines 13-14)
 export const projectCardImages = [
-  {
-    name: "example-1",
-    image: Logo,
-  },
 ];
 
 /* Contact Info
@@ -110,4 +108,4 @@ export const projectCardImages = [
   Add your formspree endpoint below.
   https://formspree.io/
 */
-export const formspreeUrl = "https://formspree.io/f/YourEndpoint";
+export const formspreeUrl = "https://formspree.io/f/mpzeqvqz";
